6c30b9a6317ce2e6c48c7a1787996340b08ffcad
burakkorki <burakkorkmaz.bilkent@gmail.com>
2020-03-30 16:31:28 +0300

    client part initialize

---
+++ project/client/node_modules/primereact/components/chips/Chips.d.ts
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import TooltipOptions from '../tooltip/TooltipOptions';
+
+interface ChipsProps {
+    id?: string;
+    name?: string;
+    placeholder?: string;
+    value?: any[];
+    max?: number;
+    disabled?: boolean;
+    style?: object;
+    className?: string;
+    tooltip?: any;
+    tooltipOptions?: TooltipOptions;
+    itemTemplate?(item: any): JSX.Element | undefined;
+    onAdd?(e: {originalEvent: Event, value: any}): void;
+    onRemove?(e: {originalEvent: Event, value: any}): void;
+    onChange?(e: {originalEvent: Event, value: any}): void;
+    onFocus?(event: Event): void;
+    onBlur?(event: Event): void;
+}
+
+export class Chips extends React.Component<ChipsProps,any> {}
---
+++ project/client/node_modules/primereact/components/dialog/Dialog.d.ts
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+interface DialogProps {
+    id?: string;
+    header?: any;
+    footer?: any;
+    visible?: boolean;
+    modal?: boolean;
+    contentStyle?: object;
+    closeOnEscape?: boolean;
+    dismissableMask?: boolean;
+    rtl?: boolean;
+    closable?: boolean;
+    style?: object;
+    className?: string;
+    showHeader?: boolean;
+    appendTo?: HTMLElement;
+    baseZIndex?: number;
+    maximizable?: boolean;
+    blockScroll?: boolean;
+    ariaCloseIconLabel?: string;
+    onHide(): void;
+    onShow?(): void;
+    iconsTemplate?(): JSX.Element | undefined;
+}
+
+export class Dialog extends React.Component<DialogProps,any> {}
